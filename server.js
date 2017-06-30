1d8a553d503b3282e9bb4944a829a4d2f08d55fe
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-06-30 15:26:49 +0100

    install bodyParser

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(require("./controllers/index"));
+
 app.use(express.static("client/build"));
 
-app.get('/', function( req, res) {
-  res.sendFile(__dirname + "/client/build/index.html")
-})
+
+// app.get('/', function( req, res) {
+//   res.sendFile(__dirname + "/client/build/index.html")
+// })
+
 app.listen(3000, function () {
   console.log( " listening on 3000" );
 })
