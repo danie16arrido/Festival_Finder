5ede93412f7454d7c3e161a43e2338d8f3f40d93
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-06-30 20:36:13 +0100

    add index.js to controllers folder and set man route

--- client/controllers/index.js
+++ client/controllers/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
-
-router.get('/', function(req, res){
-  res.json("data");
-})
+router.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/../build/index.html'));
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,10 +5,9 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(require("./controllers/index"));
+app.use(require("./client/controllers/index"));
 
 app.use(express.static("client/build"));
-
 
 // app.get('/', function( req, res) {
 //   res.sendFile(__dirname + "/client/build/index.html")
