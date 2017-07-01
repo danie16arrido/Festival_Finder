eb4af1329c3e1e2d15d194d24ad91ccbc1a501f4
Irma Rasikeviciute <rasikeviciute@yahoo.com>
2017-07-01 11:53:15 +0100

    Clear app.js

--- client/src/app.js
+++ client/src/app.js
@@ -1,13 +1,1 @@
 
-var phq = require('predicthq')
-
-client = new phq.Client({access_token: "4ih8DjKchU0joNwr0onOB4yysU4crC"})
-
-client.events.search({q: 'katy+perry'})
-    .then(function(results){
-        var events = results.toArray()
-        for(var i=0; i < events.length; i++)
-            console.info(events[i].rank, events[i].category, events[i].title, events[i].start, events[i].location )
-    })
-
-console.log("hi");
