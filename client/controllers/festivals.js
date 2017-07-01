2f5b1cf3e447c80d9149e0451167789b7600bab4
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-07-01 19:38:13 +0100

    add query to SHOW route

--- client/controllers/festivals.js
+++ client/controllers/festivals.js
@@ -4,13 +4,38 @@ var festivalsRouter = express.Router();
 var FestivalsQuery = require('../../db/festival_query.js');
 var query = new FestivalsQuery();
 
-//country index
-festivalsRouter.get('/', function(req, res) {
+//festival INDEX
+festivalsRouter.get('/', function( req, res ) {
   query.all( function ( festivals ) {
     res.json( festivals );
   })
 });
 
+//festival SHOW
+festivalsRouter.get("/:id", function(req,res) {
+  query.all( function( festivals ) {
+  res.json( festivals[req.params.id] );
+  })
+});
+
+//festival CREATE
+// festivalsRouter.post("/", function(req,res) {
+//
+//   var newFestival = new Festival(
+//   {
+//     title: req.body.title,
+//     description: req.body.description,
+//     type: req.body.type,
+//     start: req.body.start,
+//     end: req.body.end,
+//     country: req.body.country,
+//     position: req.body.position
+//   })
+//
+//   query.add(newFestival, function(festivals) {
+//     res.json(festivals);
+//   })
+// })
 
 
 module.exports = festivalsRouter;
