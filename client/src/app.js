fa3f7275da45125789d694e945aebee7744b732a
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-07-02 18:04:58 +0100

    add api call via xmlhttprequest

--- client/src/app.js
+++ client/src/app.js
@@ -1,10 +1,21 @@
 var initialize = function(){
+
   var mapDiv = document.getElementById('main-map');
   var center = { lat: 0, lng: 0 };
 
+  var MapWrapper = require('./MapWrapper');
   var mainMap = new MapWrapper(mapDiv, center, 2);
+
+  //
+  var FestivalsQuery =  require("./FestivalsList");
+  url = 'http://localhost:3000/api/festivals';
+  var list = new FestivalsQuery( url );
+  list.getData( function() {
+    for( ele of list.festivals){
+      console.log(ele.position)
+    }
+  });
+
 }
 
 window.addEventListener('load', initialize);
-
-
