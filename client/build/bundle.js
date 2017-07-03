9b090dbcc9ed0953c8d59840e1b6c24c421d6182
Irma Rasikeviciute <rasikeviciute@yahoo.com>
2017-07-03 20:46:03 +0100

    List all festivals(image, location, date) and be able to scroll them

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -190,31 +190,57 @@ var ResultsFestivals = function() {
   var FestivalsList = __webpack_require__(3);
   var url = 'http://localhost:3000/api/festivals'
   this.list = new FestivalsList(url);
-
+  // this.selectedIndex = 0;
 }
 
 ResultsFestivals.prototype = {
 
   render: function(){
 
-    var festivalList = document.getElementById('festivals');
+    var festivalList = document.getElementById('festival-div');
+    
     this.list.getData(function() {
       this.list.festivals.forEach(function(ele){
+        var festival = document.createElement('div');
+        
         var image = document.createElement('img');
-        image.classList.add('festival-image')
-        console.log(ele.image);
+        var h3 = document.createElement('h3');
+        var country = document.createElement('p');
+        var date = document.createElement('p');
+
+        image.classList.add('festival-image');
         image.src = ele.image;
-        festivalList.appendChild(image);
+
+        h3.classList.add('festival-title');
+        h3.innerText = ele.title;
+
+        country.classList.add('festival-country');
+        country.innerText = ele.country;  
+
+        date.classList.add('festival-date');
+        date.innerText = ele.start;
+
+
+        festival.classList.add('festival');
+        
+        festival.appendChild(image);
+        festival.appendChild(h3);
+        festival.appendChild(country);
+        festival.appendChild(date);
+
+        
+        festivalList.appendChild(festival);
       }.bind(this));
     }.bind(this));
-  
+
+    
 
   }
 
 }
 
 
- module.exports = ResultsFestivals;
+module.exports = ResultsFestivals;
 
 
 /***/ })
