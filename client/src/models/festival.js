55ee0a1c6f7afb8532b6c06693e2c0d20091daf1
Irma Rasikeviciute <rasikeviciute@yahoo.com>
2017-07-03 11:14:13 +0100

    Add rating to Festival model

--- client/src/models/festival.js
+++ client/src/models/festival.js
@@ -6,6 +6,7 @@ var Festival = function(options) {
   this.end = options.end;
   this.country = options.country;
   this.position = options.position;
+  this.rating = options.rating;
 }
 
 module.exports = Festival;
