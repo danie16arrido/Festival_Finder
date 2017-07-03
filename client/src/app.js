2c7318a0360dcb7ee3809f8fd25be038b8de238a
Marilena Alexe <marilena.alexe@gmail.com>
2017-07-03 18:13:52 +0100

    Add buttons (festival types) to the search area in the app.js file

--- client/src/app.js
+++ client/src/app.js
@@ -5,9 +5,36 @@ var initialize = function(){
   var MapWrapper = require('./mapWrapper');
   var mainMap = new MapWrapper(mapDiv, center, 2);
 
+  var types = ["Film", "Music", "Carnival", "Religious/Traditional", "New Year", "Food and Drink", "Featured"];
+
   mainMap.addAllMarkers();
 
+  // searchButtons.render();
+
+  // var SearchButton = require('./searchButton.js')
+  // var buttons = new SearchButton();
+
+  var renderAllButtons = function(){
+    types.forEach(function(type) {
+      renderButton(type);
+    })
+  }
+
+  var renderButton = function(type) {
+    var appendDiv = document.createElement('appendDiv');
+    appendDiv.classList.add('search-div');
+    
+    var button = document.createElement('button');
+    button.innerText = type + " Festivals";
+
+    appendDiv.appendChild(button);
+
+    var searchDiv = document.getElementById('search-bar');
+    searchDiv.appendChild(appendDiv);
+  }
+  renderAllButtons();
 }
 
 
 window.addEventListener('load', initialize);
+
