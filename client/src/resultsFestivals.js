7702735a3cea7172e2e6b15feef532d19cd1c5c4
Irma Rasikeviciute <rasikeviciute@yahoo.com>
2017-07-04 15:17:50 +0100

    Add a button to add festival to favourites

--- client/src/resultsFestivals.js
+++ client/src/resultsFestivals.js
@@ -13,22 +13,29 @@ ResultsFestivals.prototype = {
     
     this.list.getData(function() {
       this.list.festivals.forEach(function(ele){
+        //festival container
         var festival = document.createElement('div');
         
+        //festival container's body
         var image = document.createElement('img');
         var h3 = document.createElement('h3');
         var country = document.createElement('p');
         var date = document.createElement('p');
-
+       
+       //overlay container and it's body
         var overlay = document.createElement('div');
         var overlayBody = document.createElement('div');
 
+        //overlayBody container's body
         var h2 = document.createElement('h3');
         var description = document.createElement('p');
         var countryOverlay = document.createElement('p');
         var dateOverlay = document.createElement('p');
+        var favButton = document.createElement('button');
 
+        festival.classList.add('festival');
 
+        //festival's container's body
         image.classList.add('festival-image');
         image.src = ele.image;
 
@@ -42,32 +49,35 @@ ResultsFestivals.prototype = {
         date.innerText = ele.start;
 
         overlay.classList.add('overlay');
-        overlayBody.classList.add('overlayBody');
-        
+        overlayBody.classList.add('overlay-body');
+
+        //overlayBody container's body
         h2.innerText = ele.title;
         description.innerText = ele.description;
         countryOverlay.innerText = ele.country;
         dateOverlay.innerText = ele.start + "-" + ele.end;
+        favButton.innerText = "Add to favourites";
 
-        overlayBody.appendChild(h2);
-        overlayBody.appendChild(description);
-        overlayBody.appendChild(countryOverlay);
-        overlayBody.appendChild(dateOverlay);
+        favButton.classList.add('fav-button');
 
-
-        festival.classList.add('festival');
         
         festival.appendChild(image);
         festival.appendChild(h3);
         festival.appendChild(country);
         festival.appendChild(date);
 
+        overlayBody.appendChild(h2);
+        overlayBody.appendChild(description);
+        overlayBody.appendChild(countryOverlay);
+        overlayBody.appendChild(dateOverlay);
+        overlayBody.appendChild(favButton);
+        
         overlay.appendChild(overlayBody);
 
+        festival.appendChild(overlay);
         
         festivalList.appendChild(festival);
-        festival.appendChild(overlay);
-
+    
       }.bind(this));
     }.bind(this));
 
