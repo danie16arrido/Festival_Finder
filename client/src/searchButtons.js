7c0291a0b7006970dfa22268fec29992b0255371
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-07-04 14:46:43 +0100

    generate url for API call

--- client/src/searchButtons.js
+++ client/src/searchButtons.js
@@ -8,8 +8,9 @@ SearchButton.prototype = {
   renderButton: function(type) {
     var appendDiv = document.createElement('appendDiv');
     appendDiv.classList.add('search-div');
-    
+
     var button = document.createElement('button');
+    button.value = type;
     button.innerText = type + " Festivals";
 
     button.addEventListener('click', this.handleButtonClick);
@@ -26,14 +27,23 @@ SearchButton.prototype = {
     }.bind(this))
   },
 
-  handleButtonClick: function() {
-   console.log("button clicked");
+  handleButtonClick: function( event ) {
+    var ResultsFestivals = require('./resultsFestivals.js');
+    var results = new ResultsFestivals();
+    var apiCall = "http://localhost:3000/api/festivals/type/";
+    var type = (event.target.value).toString();
+    type = type.split(' ').join('_');
+    apiCall += type;
+    console.log( apiCall  );
+
+    // results.renderSliderFestivals( url );
+
   },
 
   renderFavButton: function(fav) {
     var appendDiv = document.createElement('appendDiv');
     appendDiv.classList.add('head-div');
-    
+
     var button = document.createElement('button');
     button.innerText = " Favourites";
 
